6ffbd0d4e7f1e7463e1c08c88ce43a01bcf58df0
C <earthhominid@gmail.com>
2022-08-04 16:56:42 -0300

    add article feature

---
+++ components/add-template/Styles.ts
@@ -0,0 +1,110 @@
+import tw from 'tailwind-styled-components';
+
+export const FormPageTitle = tw.h1`
+  px-2 
+  mt-5 
+  mb-4 
+  text-2xl 
+  font-montserrat
+  selection:font-bold 
+  text-zinc-700 
+  text-center
+`;
+
+export const FormContainer = tw.div`
+  flex 
+  w-full 
+  justify-center
+
+`;
+
+export const InsideContainer = tw.div`
+  w-full
+  py-6
+  flex
+  justify-center
+  bg-white
+  border 
+  border-slate-300 
+  shadow-lg
+  rounded-md
+  `;
+
+export const InputLabelContainer = tw.div`
+  relative 
+  mt-4
+ 
+`;
+
+export const StyledInput = tw.input`
+  flex
+  flex-1
+  h-12
+  peer
+  border-b-2 
+  border-gray-300 
+  focus:outline-none 
+  focus:border-blue-500 placeholder-transparent
+  invalid:border-pink-500
+  invalid:text-pink-600
+  focus:invalid:border-pink-500
+  focus:invalid:ring-pink-500
+`;
+
+export const StyledTextArea = tw.textarea`
+  flex
+  flex-1
+  h-12
+  peer
+  border-b-2 
+  border-gray-300 
+  focus:outline-none 
+  focus:border-blue-500 placeholder-transparent
+  invalid:border-pink-500
+  invalid:text-pink-600
+  focus:invalid:border-pink-500
+  focus:invalid:ring-pink-500
+`;
+
+export const WideTextArea = tw.textarea`
+  block 
+  p-2.5 
+  w-full 
+  text-sm 
+  text-gray-900 
+  bg-gray-50 
+  rounded-lg border 
+  border-gray-300 
+  focus:ring-blue-500 
+  focus:border-blue-500 
+  
+`;
+
+export const StyledLabel = tw.label`
+text-stone-800 
+  absolute 
+  left-0  
+  -top-3.5 
+  text-sm
+  md:text-base
+  peer-placeholder-shown:text-base peer-placeholder-shown:text-stone-600 peer-placeholder-shown:top-2 transition-all 
+  peer-focus:-top-3.5
+  peer-foucs:text-stone-600
+  peer-focus:text-sm
+`;
+
+export const FormButton = tw.button`
+  bg-blue-500 
+  p-2 
+  rounded-md 
+  text-white 
+  w-full 
+  text-base 
+  hover:bg-white
+  border-2
+  border-blue-500
+  hover:text-blue-500
+  ease-in-out
+  transition
+  duration-300
+`;
