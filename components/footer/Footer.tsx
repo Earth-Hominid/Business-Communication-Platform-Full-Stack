6523f5c14bfbe486c1485131a42bf6bc4243aa0c
C <earthhominid@gmail.com>
2022-08-19 12:51:33 -0300

    changed footer alias to real name

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -3,7 +3,7 @@ import { FooterContainer, FooterText } from './Styles';
 const Footer = () => {
   return (
     <FooterContainer>
-      <FooterText>Copyright Earth-Hominid 2022</FooterText>
+      <FooterText>Copyright Chris Doll 2022</FooterText>
     </FooterContainer>
   );
 };
